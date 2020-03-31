97e650e178bc5ad587dffc8a027b004fe2e7ed34
Joy M <magbanuamjm@gmail.com>
2020-03-31 18:06:37 -0400

    Initial commit

---
+++ src/chapter4/GrossPay.java
@@ -0,0 +1,21 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class GrossPay {
+
+    private static final int EMPRATE = 15;
+    private stati
+    private Scanner scanner;
+
+    public static void main(String args[]){
+        scanner = new Scanner(System.in);
+        getWeek
+    }
+
+    public int getWeeklyHrs(int hrs){
+        System.out.println("Input weekly hours: ");
+        int hrs = scanner.nextInt();
+        return hrs1;
+    }
+}
---
+++ src/chapter9/Inheritance.java
@@ -0,0 +1,8 @@
+package chapter9;
+
+public class Inheritance {
+
+    Person person = new Person();
+    Employee employee = new Employee();
+    person.
+}
